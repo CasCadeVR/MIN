7844b129f4fd821428312ee828ef26ee6ab20cc9
Karo4a <t1mati666@yandex.ru>
2026-03-19 10:09:36 +0300

    default timeout 300ms

--- MIN.Services.Contracts/Models/Settings.cs
+++ MIN.Services.Contracts/Models/Settings.cs
@@ -10,7 +10,7 @@ namespace MIN.Services.Contracts.Models
         /// <summary>
         /// Время ожидания поиска комнаты
         /// </summary>
-        public int DiscoveryTimeout { get; set; } = 100;
+        public int DiscoveryTimeout { get; set; } = 300;
 
         /// <summary>
         /// Метод поиска комнат
