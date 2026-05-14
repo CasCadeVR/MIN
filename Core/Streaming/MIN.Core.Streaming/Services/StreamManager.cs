b3d55e5532938eb3fc5e11bf0e60e244a0d41518
Admin <admin@example.com>
2026-05-14 07:40:27 +0300

    forgot to remove delaying senfing chunk

--- Core/Streaming/MIN.Core.Streaming/Services/StreamManager.cs
+++ Core/Streaming/MIN.Core.Streaming/Services/StreamManager.cs
@@ -182,8 +182,6 @@ public sealed class StreamManager : IStreamManager, IDisposable
                 logger.Log($"Отправлен пакет {chunkIndex + 1}/{totalChunks} для потока {streamId}");
                 await transport.SendAsync(encrypted, roomId, recipientConnectionId, cancellationToken);
                 chunkIndex++;
-
-                await Task.Delay(1000);
             }
 
             CleanForStream(streamId);
