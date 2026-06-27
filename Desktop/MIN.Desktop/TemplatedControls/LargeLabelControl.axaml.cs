b5a77b991e620ba2bc3a3b890c12009637fbb42c
Admin <admin@example.com>
2026-06-27 14:52:52 +0300

    some visuals

---
+++ Desktop/MIN.Desktop/TemplatedControls/LargeLabelControl.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace MIN.Desktop.TemplatedControls;
+
+/// <summary>
+/// Большая <see cref="Label"/>
+/// </summary>
+public class LargeLabelControl : TemplatedControl
+{
+
+}
