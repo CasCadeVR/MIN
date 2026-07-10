6ee209f66a99734556615fb875219f0126476cd2
Admin <admin@example.com>
2026-07-10 10:45:24 +0300

    fixed scroll bar and made participant cards just to show

---
+++ Desktop/MIN.Desktop/ViewModels/Cards/ParticipantCardViewModel.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MIN.Chat.Events;
+using MIN.Core.Entities;
+using MIN.Core.Entities.Contracts.Enums;
+using MIN.Core.Events.Contracts;
+using MIN.Desktop.ViewModels.Base;
+
+namespace MIN.Desktop.ViewModels.Cards;
+
+/// <summary>
+/// Модель карточки участника на боковой панели
+/// </summary>
+public partial class ParticipantCardViewModel : CardViewModelBase, IDisposable
+{
+    private const string StartPrivateChatText = "Начать приватное общение";
+    private const string StopPrivateChatText = "Прекратить приватное общение";
+
+    private readonly Participant participant;
+    private readonly IEventBus eventBus;
+    private readonly Guid roomId;
+    private readonly bool isHost;
+    private readonly bool isSelf;
+    private HashSet<IDisposable> eventTokens = null!;
+
+    /// <summary>
+    /// Идентфикатор участника на карточке
+    /// </summary>
+    public Guid ParticipantId => participant.Id;
+
+    /// <summary>
+    /// Имя участника
+    /// </summary>
+    [ObservableProperty]
+    public partial string ParticipantName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Роль участника
+    /// </summary>
+    [ObservableProperty]
+    public partial string ParticipantRole { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Статус участника
+    /// </summary>
+    [ObservableProperty]
+    public partial OnlineStatus ParticipantStatus { get; set; }
+
+    /// <summary>
+    /// Время последнего онлайн
+    /// </summary>
+    [ObservableProperty]
+    public partial DateTime ParticipantLastSeenAt { get; set; } = DateTime.Now;
+
+    /// <summary>
+    /// Выбрана ли карточка
+    /// </summary>
+    [ObservableProperty]
+    public partial bool IsSelected { get; set; }
+
+    /// <summary>
+    /// Является ли сейчас пользователь не в сети
+    /// </summary>
+    [ObservableProperty]
+    public partial bool IsOffline { get; set; }
+
+    /// <summary>
+    /// Событие по нажатию на саму карточку
+    /// </summary>
+    public Action? Clicked { get; set; }
+
+    /// <summary>
+    /// Инициализирует новый экземпляр <see cref="ParticipantCardViewModel"/>
+    /// </summary>
+    public ParticipantCardViewModel(Participant participant,
+        IEventBus eventBus,
+        Guid roomId,
+        bool isHost,
+        bool isSelf,
+        bool asHost)
+    {
+        this.participant = participant;
+        this.eventBus = eventBus;
+        this.roomId = roomId;
+        this.isHost = isHost;
+        this.isSelf = isSelf;
+
+        if (!Design.IsDesignMode)
+        {
+            FillLabels();
+
+            if (!isSelf)
+            {
+                SubscribeToEvents();
+            }
+        }
+    }
+
+    private void SubscribeToEvents()
+    {
+        eventTokens =
+        [
+            eventBus.Subscribe<OnlineStatusChangedEvent>(OnOnlineStatusChanged),
+        ];
+    }
+
+    private async Task OnOnlineStatusChanged(OnlineStatusChangedEvent eventMessage, CancellationToken cancellationToken)
+    {
+        if (eventMessage.RoomId != roomId)
+        {
+            return;
+        }
+
+        if (eventMessage.SenderId == participant.Id)
+        {
+            ParticipantStatus = isSelf ? OnlineStatus.Online : eventMessage.Status;
+            IsOffline = ParticipantStatus == OnlineStatus.Offline;
+        }
+    }
+
+    /// <summary>
+    /// Отменить выбор карточки
+    /// </summary>
+    public void Unselect()
+    {
+        IsSelected = false;
+        UpdateStylesOutOfSelected();
+    }
+
+    /// <summary>
+    /// Выбрать карточку
+    /// </summary>
+    public void SelectCard()
+    {
+        IsSelected = true;
+    }
+
+    /// <summary>
+    /// Выбрать карточку
+    /// </summary>
+    [RelayCommand]
+    public void SelectItem()
+    {
+        Clicked?.Invoke();
+    }
+
+    //private void OnPrivateChatClickMenuStripClicked()
+    //{
+    //    selected = !selected;
+    //    UpdateStylesOutOfSelected();
+    //    OnPrivateChatMenuStripClicked?.Invoke(selected, participant);
+    //}
+
+    //private void OnKickParticipantClickMenuStripClicked()
+    //{
+    //    OnKickParticipantClicked?.Invoke(participant);
+    //}
+
+    private void UpdateStylesOutOfSelected()
+    {
+        //ContextMenuStrip?.Items[0].Text = IsSelected ? StopPrivateChatText : StartPrivateChatText;
+        //BackColor = IsSelected
+        //    ? ColorScheme.PrivateParticipantCardBackground
+        //    : ColorScheme.DefaultParticipantCardBackground;
+    }
+
+    private void FillLabels()
+    {
+        ParticipantName = participant.Name;
+        ParticipantStatus = isSelf ? OnlineStatus.Online : participant.CurrentStatus;
+        IsOffline = ParticipantStatus == OnlineStatus.Offline;
+
+        if (isHost)
+        {
+            ParticipantRole = "Хост";
+        }
+        else
+        {
+            ParticipantRole = "";
+        }
+    }
+
+    /// <inheritdoc cref="IDisposable.Dispose"/>
+    void IDisposable.Dispose()
+    {
+        foreach (var token in eventTokens)
+        {
+            token.Dispose();
+        }
+    }
+}
--- Desktop/MIN.Desktop/ViewModels/Pages/ChatSideBarViewModel.cs
+++ Desktop/MIN.Desktop/ViewModels/Pages/ChatSideBarViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Avalonia.Collections;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MIN.Core.Entities;
@@ -8,6 +9,8 @@ using MIN.Desktop.Contracts.Constants;
 using MIN.Desktop.Contracts.Enums;
 using MIN.Desktop.Infrastructure.Services;
 using MIN.Desktop.ViewModels.Base;
+using MIN.Desktop.ViewModels.Cards;
+using MIN.DI.FeatureCollection;
 
 namespace MIN.Desktop.ViewModels.Pages;
 
@@ -16,6 +19,7 @@ namespace MIN.Desktop.ViewModels.Pages;
 /// </summary>
 public partial class ChatSideBarViewModel : RoutableViewModelBase
 {
+    private IMinFeatureCollection featureCollection;
     private ParticipantInfo localParticipant = null!;
     private Guid roomId;
 
@@ -32,10 +36,22 @@ public partial class ChatSideBarViewModel : RoutableViewModelBase
     public override EventHandler? OnNavigatedFrom { get; }
 
     /// <summary>
+    /// Участники комнаты
+    /// </summary>
+    [ObservableProperty]
+    public partial AvaloniaList<ParticipantCardViewModel> RoomParticipants { get; set; } = [];
+
+    /// <summary>
     /// Комната
     /// </summary>
     [ObservableProperty]
     public partial Room Room { get; set; } = null!;
+
+    /// <summary>
+    /// Имя хоста
+    /// </summary>
+    [ObservableProperty]
+    public partial string HostName { get; set; } = string.Empty;
 
     /// <summary>
     /// IP Адрес
@@ -56,6 +72,12 @@ public partial class ChatSideBarViewModel : RoutableViewModelBase
     public partial string Classroom { get; set; } = string.Empty;
 
     /// <summary>
+    /// Являяется локальный пользователь хостом
+    /// </summary>
+    [ObservableProperty]
+    public partial bool IsHost { get; set; }
+
+    /// <summary>
     /// Открыта ли панель
     /// </summary>
     public bool IsOpened { get; set; }
@@ -63,8 +85,10 @@ public partial class ChatSideBarViewModel : RoutableViewModelBase
     /// <summary>
     /// Инициализирует новый экземпляр <see cref="ChatSideBarViewModel"/>
     /// </summary>
-    public ChatSideBarViewModel()
+    public ChatSideBarViewModel(IMinFeatureCollection featureCollection)
     {
+        this.featureCollection = featureCollection;
+
         OnNavigatedTo = (sender, e) => IsOpened = true;
         OnNavigatedFrom = (sender, e) =>
         {
@@ -84,13 +108,74 @@ public partial class ChatSideBarViewModel : RoutableViewModelBase
         Room = room;
         roomId = room.Id;
 
-        if (IpAddressParser.TryParseIpAddress(room.ConnectionAddress, out var gottenIpAddress, out var port))
+        UpdateStats();
+    }
+
+    private void UpdateStats()
+    {
+        IsHost = Room.HostParticipant?.Id == localParticipant.Id;
+        HostName = IsHost ? "Ты" : Room.HostParticipant?.Name ?? "Неизвестно";
+
+        if (IpAddressParser.TryParseIpAddress(Room.ConnectionAddress, out var gottenIpAddress, out var port))
         {
             Port = port.ToString();
             IpAddress = gottenIpAddress;
         }
 
-        Classroom = string.IsNullOrEmpty(room.Cabinet) ? DesktopConstants.UndefinedPcName : room.Cabinet;
+        Classroom = string.IsNullOrEmpty(Room.Cabinet) ? DesktopConstants.UndefinedPcName : Room.Cabinet;
+
+        UpdateParticipantFlow();
+    }
+
+    private void UpdateParticipantFlow()
+    {
+        RoomParticipants.Clear();
+
+        foreach (var participant in Room.CurrentParticipants)
+        {
+            var card = new ParticipantCardViewModel(participant,
+                featureCollection.Core.EventBus,
+                roomId,
+                isHost: participant.Id == Room.HostParticipant.Id,
+                isSelf: participant.Id == localParticipant.Id,
+                asHost: localParticipant.Id == Room.HostParticipant.Id);
+
+            //card.OnPrivateChatMenuStripClicked += (selected, particpant) =>
+            //{
+            //    foreach (var participantsFlowControl in participantsFlow.Controls)
+            //    {
+            //        if (participantsFlowControl is ParticipantCard participantCard)
+            //        {
+            //            if (participantCard.ParticipantId != participant.Id)
+            //            {
+            //                participantCard.Unselect();
+            //            }
+            //        }
+            //    }
+
+            //    privateChatParticipantId = selected ? participant.Id : null;
+            //};
+
+            //card.OnKickParticipantClicked += async (participant) =>
+            //{
+            //    var kickForm = new ParticipantKickForm(participant.Name);
+            //    if (kickForm.ShowDialog() == DialogResult.OK)
+            //    {
+            //        try
+            //        {
+            //            await featureCollection.Chat.ChatRoomService.KickParticipantAsync(roomId,
+            //            participant.Id, kickForm.Reason, formCts.Token);
+            //        }
+            //        catch (Exception ex)
+            //        {
+            //            MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //        }
+            //    }
+            //};
+
+            RoomParticipants.Add(card);
+        }
+        //participantsInfo.Text = $"{room.ParticipantCount}/{room.MaximumParticipants}";
     }
 
     /// <summary>
