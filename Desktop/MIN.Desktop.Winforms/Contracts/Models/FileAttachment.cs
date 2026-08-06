4f1e8474f2a569158ddf28c9a8363b90ac8668de
CasCadeVR <girmilend@gmail.com>
2026-08-06 11:08:05 +0300

    Removed winforms project

--- Desktop/MIN.Desktop.Winforms/Contracts/Models/FileAttachment.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MIN.Desktop.Contracts.Models;
-
-/// <summary>
-/// Приложение файла
-/// </summary>
-/// <param name="FileName">Имя файла</param>
-/// <param name="FilePath">Путь к файлу</param>
-public record struct FileAttachment(string FileName, string FilePath) { }
